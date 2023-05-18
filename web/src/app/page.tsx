4ce6580826ec40bc205ba0c2e900291536beed81
juliastefanoni <juliastefanonidev@gmail.com>
2023-05-18 13:34:09 -0300

    feat: add fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="h-screen bg-zinc-950 text-4xl font-bold text-zinc-50">
-      Hello world!
-    </h1>
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Hello world!</h1>
+      <h1 className="font-alt text-4xl font-bold">Hello world 2!</h1>
+    </div>
   )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+        alt: 'var(--font-bai-jamjuree)',
+      },
     },
   },
   plugins: [],
